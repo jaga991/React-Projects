a2a378216af9d92749cb160ef715f3c2ef2e2f15
Chee Kiat <jaga991@live.com>
2023-01-16 13:45:24 +0800

    working on showing the decks on landing page

--- FLASHCARDUSAGE/server/src/index.ts
+++ FLASHCARDUSAGE/server/src/index.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 config()
 import express, {Request, Response} from "express";
 import mongoose from 'mongoose';
-
+import cors from 'cors';
 
 
 import Deck from './models/Deck'
@@ -10,9 +10,17 @@ import Deck from './models/Deck'
 const PORT = 5000;
 
 const app = express();
-
+//middleware
+//bypass cors error locally
+app.use(cors()); //default argument is '*'
 //allow support for json post request
 app.use(express.json())
+
+app.get("/decks", async (req: Request, res: Response) => {
+    //TODO, fetch all decks and send back to the user
+    const decks = await Deck.find() //get Deck collection from mongodb
+    res.json(decks)
+})
 
 app.post("/decks", async (req: Request, res: Response) => {
     console.log(req.body)
@@ -23,18 +31,9 @@ app.post("/decks", async (req: Request, res: Response) => {
     res.json(createdDeck)
 });
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("gg");
-    
-});
-
-app.get("/hello", (req:Request,res:Response) => {
-    res.send("hello world");
-});
-
 const db = mongoose.connect(process.env.MONGO_URL!)
 .then(() => {
     console.log(`listening on port ${PORT}`);
     app.listen(PORT);
 });
-
+ 
